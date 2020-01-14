a427ec1a9f763a13e0f6a56aeff0ae3366554f44
Sumit Kawade @ Home <kawadesumit@gmail.com>
2020-01-13 21:59:24 -0500

    serialization added serialization not suppoted support

--- src/serialization.jl
+++ src/serialization.jl
@@ -7,15 +7,29 @@ function deserialize(req::String, type)
         deserializeSupply(req)
     elseif type == Demand
         deserializeDemand(req)
+    else
+        throw("Not Supported")
     end
 end
 
+# TODO mapping using reflection aka fields in stead of explicit mapping
+
 function deserializeSupply(req::String)::Supply
     reqDict = JSON.parse(req)
-    Supply(reqDict["itemId"], reqDict["uom"], reqDict["nodeId"], reqDict["type"], reqDict["quantity"])
+    Supply(
+        reqDict["itemId"],
+        reqDict["uom"],
+        reqDict["nodeId"],
+        reqDict["type"],
+        reqDict["quantity"])
 end
 
 function deserializeDemand(req::String)::Demand
     reqDict = JSON.parse(req)
-    Demand(reqDict["itemId"], reqDict["uom"], reqDict["nodeId"], reqDict["type"], reqDict["quantity"])
+    Demand(
+        reqDict["itemId"],
+        reqDict["uom"],
+        reqDict["nodeId"],
+        reqDict["type"],
+        reqDict["quantity"])
 end
--- test/serializationTest.jl
+++ test/serializationTest.jl
@@ -5,24 +5,34 @@ demandTemp = Demand("item00001", "each", "node001", "allocated", 10)
 
 
 @testset "Serialization Tests" begin
-    supplyString = "{\"itemId\":\"supplyItem00003\",\"uom\":\"each\",\"nodeId\":\"node001\",\"type\":\"onhand\",\"quantity\":100}"
-    supply = deserialize(supplyString, Supply)
-    @info supply
 
-    @test supply.itemId == "supplyItem00003"
-    @test supply.uom == "each"
-    @test supply.nodeId == "node001"
-    @test supply.type == "onhand"
-    @test supply.quantity == 100
+    @testset "Supply test" begin
+        supplyString = "{\"itemId\":\"supplyItem00003\",\"uom\":\"each\",\"nodeId\":\"node001\",\"type\":\"onhand\",\"quantity\":100}"
+        supply = deserialize(supplyString, Supply)
+        @info supply
 
-    demandString = "{\"itemId\":\"demandItem00003\",\"uom\":\"each\",\"nodeId\":\"node001\",\"type\":\"allocated\",\"quantity\":100}"
-    demand = deserialize(demandString, Demand)
-    @info demand
+        @test supply.itemId == "supplyItem00003"
+        @test supply.uom == "each"
+        @test supply.nodeId == "node001"
+        @test supply.type == "onhand"
+        @test supply.quantity == 100
+    end
 
-    @test demand.itemId == "demandItem00003"
-    @test demand.uom == "each"
-    @test demand.nodeId == "node001"
-    @test demand.type == "allocated"
-    @test demand.quantity == 100
+    @testset "Demand test" begin
+        demandString = "{\"itemId\":\"demandItem00003\",\"uom\":\"each\",\"nodeId\":\"node001\",\"type\":\"allocated\",\"quantity\":100}"
+        demand = deserialize(demandString, Demand)
+        @info demand
+
+        @test demand.itemId == "demandItem00003"
+        @test demand.uom == "each"
+        @test demand.nodeId == "node001"
+        @test demand.type == "allocated"
+        @test demand.quantity == 100
+    end
+
+    @testset "Not supported" begin
+        inputString = "{\"itemId\":\"demandItem00003\",\"uom\":\"each\",\"nodeId\":\"node001\",\"type\":\"allocated\",\"quantity\":100}"
+        @test_throws "Not Supported" deserialize(inputString, Set)
+    end
 
 end
