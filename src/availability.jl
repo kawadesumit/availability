486bf81afc0a09219d71c41d21ed55cda475ff56
Sumit Kawade @ Home <kawadesumit@gmail.com>
2020-01-18 21:08:55 -0500

    commented router include so that unit tests can run

--- src/availability.jl
+++ src/availability.jl
@@ -5,7 +5,7 @@ export Supply, Demand, calculate_Avail
 include("model/supply.jl")
 include("model/demand.jl")
 include("serialization.jl")
-include("router.jl")
+# include("router.jl")
 
 function calculate_Avail(supply::Supply, demand::Demand)::Int128
     @info ("calculate availability")
